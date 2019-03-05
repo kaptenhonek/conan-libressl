3a5dbc10f79a356b8b29bc0a02888f3f128ed574
Henrik Jakobsson Majava <henrik@bobby.se>
2019-03-05 13:58:03 +0100

    updated libressl to v2.8.3

--- conanfile.py
+++ conanfile.py
@@ -5,7 +5,7 @@ from glob import glob
 
 class LibreSSLConan(NxConanFile):
     name = "libressl"
-    version = "2.5.3"
+    version = "2.8.3"
     license = "OpenBSD"
     url = "https://github.com/hoxnox/conan-libressl"
     license = "https://github.com/libressl/libressl/blob/master/src/LICENSE"
@@ -16,7 +16,7 @@ class LibreSSLConan(NxConanFile):
     description = "LibreSSL is a version of the TLS/crypto stack forked from OpenSSL in 2014, with goals of modernizing the codebase, improving security, and applying best practice development processes."
 
     def do_source(self):
-        self.retrieve("14e34cc586ec4ce5763f76046dcf366c45104b2cc71d77b63be5505608e68a30",
+        self.retrieve("9b640b13047182761a99ce3e4f000be9687566e0828b4a72709e9e6a3ef98477",
                 [
                     "vendor://openbsd/libressl/libressl-{v}.tar.gz".format(v=self.version),
                     "https://ftp.openbsd.org/pub/OpenBSD/LibreSSL/libressl-{v}.tar.gz".format(v=self.version)
@@ -27,12 +27,14 @@ class LibreSSLConan(NxConanFile):
         tools.untargz("libressl-{v}.tar.gz".format(v=self.version), "{staging_dir}/src".format(staging_dir=self.staging_dir))
         src_dir = "{staging_dir}/src/libressl-{v}".format(staging_dir=self.staging_dir, v=self.version)
         cmake.build_dir = "{src_dir}/build".format(src_dir=src_dir)
-        for file in sorted(glob("patch/*.patch")):
-            self.output.info("Applying patch '{file}'".format(file=file))
-            tools.patch(base_path=src_dir, patch_file=file, strip=0)
+        # for file in sorted(glob("patch/*.patch")):
+        #     self.output.info("Applying patch '{file}'".format(file=file))
+        #     tools.patch(base_path=src_dir, patch_file=file, strip=0)
         cmake_defs = {"CMAKE_INSTALL_PREFIX": self.staging_dir,
                       "CMAKE_INSTALL_LIBDIR": "lib",
-                      "BUILD_SHARED": "1" if self.options["libressl"].shared else "0"}
+                      "BUILD_SHARED": "1" if self.options["libressl"].shared else "0",
+                      "LIBRESSL_APPS": "OFF",
+                      "LIBRESSL_TESTS": "OFF"}
         cmake_defs.update(self.cmake_crt_linking_flags())
         cmake.configure(defs=cmake_defs, source_dir=src_dir)
         cmake.build(target="install")
@@ -45,9 +47,5 @@ class LibreSSLConan(NxConanFile):
                     "crypto-shared" if self.options["libressl"].shared else "crypto.lib"
                 ]
         else:
-            self.cpp_info.libs = [
-                    "tls.so" if self.options["libressl"].shared else "tls.a",
-                    "ssl.so" if self.options["libressl"].shared else "ssl.a",
-                    "crypto.so" if self.options["libressl"].shared else "crypto.a"
-                ]
+            self.cpp_info.libs = ["tls", "ssl", "crypto"]
 
